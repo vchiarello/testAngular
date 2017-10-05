d69608b4da7b944101ebebd2876f88fee01915a7
vchiarello <vito.chiarello@gmail.com>
2017-10-05 17:06:36 +0200

    Albero affinato senza info sulle foglie

--- js/angular/controller/testController.js
+++ js/angular/controller/testController.js
@@ -7,25 +7,15 @@ angular.module("richiesteReteApp").controller("testController", ['$scope', '$htt
 	
 	init();
 
-   $http.get('json/contentTest.json').success(function(response) {
+   $http.get('json/contentTest2.json').success(function(response) {
         $scope.risultato= response.data;
     });
 
     $scope.toggle = function(n){
-        n.aperto=!n.aperto;
-        if (n.imgAlbero == "./images/chiuso.png")
-            n.imgAlbero = "./images/aperto.png"
-        else
-            n.imgAlbero = "./images/chiuso.png"
+        if ( n.figli.length>0)
+            n.aperto=!n.aperto;
     }
 
-    $scope.fogliaSelezionata = function(f){
-        return fSelezionata(f);
-    }
-
-    function fSelezionata(f){
-        return f.selezionato;
-    }
 
     $scope.imgSelezioneNodo = function(n){
         if (statoSelezioneFigli(n)==1) return "./images/TuttoSelezione.png";
@@ -34,11 +24,19 @@ angular.module("richiesteReteApp").controller("testController", ['$scope', '$htt
     }
 
     function statoSelezioneFigli(n){
+        if ((n.figli === undefined || n.figli == null || n.figli.length==0) && n.selezionato)
+            return 1;
+        else if ((n.figli === undefined || n.figli == null || n.figli.length==0) && !n.selezionato)    
+            return -1;
+
         var tuttiSelezionati = true;
         var almenoUnoSelezionato = false;
-        for (i = 0; i < n.foglie.length;i++){
-            if (tuttiSelezionati && !n.foglie[i].selezionato)tuttiSelezionati = false;
-            if (n.foglie[i].selezionato)almenoUnoSelezionato = true;
+        
+        for (var i = 0; i < n.figli.length;i++){
+            sFigli = statoSelezioneFigli(n.figli[i]);
+
+            if (tuttiSelezionati && sFigli!=1)tuttiSelezionati = false;
+            if (sFigli!=-1)almenoUnoSelezionato = true;
         }
 
         if (tuttiSelezionati)return 1;
@@ -48,29 +46,47 @@ angular.module("richiesteReteApp").controller("testController", ['$scope', '$htt
     }
 
     $scope.cambiaSelezioneNodo = function(n){
-        if (statoSelezioneFigli(n) == 1)
-            for (i = 0; i < n.foglie.length; i++)n.foglie[i].selezionato=false;
-        else    
-            for (i = 0; i < n.foglie.length; i++)n.foglie[i].selezionato=true;
+        if (n.figli === undefined || n.figli == null || n.figli.length==0 ){
+            n.selezionato = ! n.selezionato;
+            return;
+        }    
 
+        if (statoSelezioneFigli(n) == 1)
+            mettiSelezioneNodo(n,0)
+        else    
+            mettiSelezioneNodo(n,1)
+        
+    }
+    
+    function mettiSelezioneNodo(n,valore){
+        if ((n.figli === undefined || n.figli == null || n.figli.length==0) && valore ==0){
+            n.selezionato = false;
+            return;
+        }
+        else if ((n.figli === undefined || n.figli == null || n.figli.length==0) && valore ==1){
+            n.selezionato = true; 
+            return;
+        }
+               
+       for (var i = 0; i < n.figli.length;i++)
+            mettiSelezioneNodo(n.figli[i],valore);
     }
 
     $scope.imgNodo = function(n){
-        if (n.imgAlbero ===undefined){
+        //se è una foglia l'immagine è foglia
+        if ((n.imgAlbero ===undefined) && (n.figli === undefined || n.figli == null || n.figli.length==0))
+            n.imgAlbero = "./images/foglia.png";
+        else if ((n.figli === undefined || n.figli == null || n.figli.length==0))
+            n.imgAlbero = "./images/foglia.png";
+        else if (n.imgAlbero ===undefined) // altrimenti c'è albero chiuso
             n.imgAlbero = "./images/chiuso.png";
+        else if (n.aperto && n.figli.length>0) // se albero aperto icona aperto
+            n.imgAlbero = "./images/aperto.png";    
+        else if (!n.aperto && n.figli.length>0){ // se albero chiuso icona chiuso
+            n.imgAlbero = "./images/chiuso.png";    
         }
+
         return n.imgAlbero;
-    }
-    
-    $scope.selezioneFoglia = function(f){
-        if (f.selezionato)
-            return "./images/TuttoSelezione.png";
-        else
-            return "./images/nessunaSelezione.png";
-    }
-    
-    $scope.cambiaSelezioneFoglia = function(f){
-        f.selezionato = !f.selezionato;
     }
     
 
